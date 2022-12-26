20e80b26d440b1e27f9bef950fc11e018ffe7f2d
Craig A. Butler <wunderhund@gmail.com>
2022-12-26 08:39:11 -0500

    Title and Navbar Working

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,10 @@ module.exports = function(eleventyConfig) {
 
   // Copy `src/images/combadge.svg` to `_site/images/combadge.svg`
   eleventyConfig.addPassthroughCopy("./src/images/combadge.svg");
+  eleventyConfig.addPassthroughCopy("./src/images/Klop.svg");
+  eleventyConfig.addPassthroughCopy("./src/images/Craig.svg");
+  eleventyConfig.addPassthroughCopy("./src/images/TerribleTrek.svg");
+  eleventyConfig.addPassthroughCopy("./src/images/background.jpg");
 
   // Copy `src/styles/main.css` to `_site/styles/main.css`
   eleventyConfig.addPassthroughCopy("./src/styles/main.css");
@@ -13,7 +17,7 @@ module.exports = function(eleventyConfig) {
   // Copy `src/fonts/*` to `_site/fonts/*`
   eleventyConfig.addPassthroughCopy("./src/fonts/*");
 
-  return {    
+  return {
     // When a passthrough file is modified, rebuild the pages:
     passthroughFileCopy: true,
     
