44e0faa96f9a0ab8452cea79af6ee5f695adad08
Craig A. Butler <wunderhund@gmail.com>
2022-12-26 17:38:09 -0500

    First Draft of Posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
 
   // Copy `src/robots.txt` to `_site/robots.txt`
@@ -17,7 +19,26 @@ module.exports = function(eleventyConfig) {
   // Copy `src/fonts/*` to `_site/fonts/*`
   eleventyConfig.addPassthroughCopy("./src/fonts/*");
 
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  });
+
   return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: "njk",
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: "njk",
+
     // When a passthrough file is modified, rebuild the pages:
     passthroughFileCopy: true,
     
